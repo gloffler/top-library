f3aaaf7bc2abb928331f192d608182ba8235b020
Gregor <174007895+gloffler@users.noreply.github.com>
2025-10-30 20:43:35 +0100

    refactor Book info function to prototype for improved performance, complete addBookToLibrary

--- library.js
+++ library.js
@@ -3,14 +3,18 @@ const library = [];
 function Book(title, author, pages, read) {
   if (!new.target)
     throw Error("You must use the 'new' operator to call the constructor");
+  this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = () => {
-    const readStatus = read ? "already read" : "not read yet";
-    return `${title} by ${author}, ${pages} pages, ${readStatus}`;
-  };
 }
 
-function addBookToLibrary() {}
+Book.prototype.info = function () {
+  const readStatus = this.read ? "already read" : "not read yet";
+  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+};
+
+function addBookToLibrary(book) {
+  library.push(book);
+}
