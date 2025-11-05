b5744aca0bacc3aed36a7accd0a8a2f203ca79c7
gloffler <>
2025-11-05 15:25:19 +0100

    refactor: constructor function to class

--- library.js
+++ library.js
@@ -7,20 +7,21 @@ const pagesInput = document.getElementById("pages");
 const readInput = document.getElementById("read");
 const tableBody = document.querySelector("table tbody");
 
-function Book(title, author, pages, read) {
-  if (!new.target)
-    throw Error("You must use the 'new' operator to call the constructor");
-  this.id = crypto.randomUUID();
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+class Book {
+  constructor(title, author, pages, read) {
+    if (!new.target)
+      throw Error("You must use the 'new' operator to call the constructor");
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+  info() {
+    const readStatus = this.read ? "already read" : "not read yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
+  }
 }
-
-Book.prototype.info = function () {
-  const readStatus = this.read ? "already read" : "not read yet";
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
-};
 
 function addBookToLibrary(book) {
   library.push(book);
